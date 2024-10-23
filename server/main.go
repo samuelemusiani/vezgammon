770ecead145941a329347ffe4a90b46e7f6ab380
Jabbar03 <ayache.omar@gmail.com>
2024-10-23 13:27:55 +0200

    setup config file

--- server/main.go
+++ server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	//"errors"
@@ -12,7 +14,19 @@ func checkServer(c *gin.Context) {
 }
 
 func main() {
+	path := "./config.toml"
+
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+
+	err := parseConf(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := getConf()
 	router := gin.Default()
 	router.GET("/api/ready", checkServer)
-	router.Run("localhost:3001")
+	router.Run(conf.Server.Bind)
 }
