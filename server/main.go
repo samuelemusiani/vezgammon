c0d355ef778b8ea1f4298b10fe76ba0964d096ee
Samuele Musiani <samu@teapot.ovh>
2024-10-29 19:01:53 +0100

    fix: vue router integration with gin

--- server/main.go
+++ server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"embed"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
@@ -31,6 +33,22 @@ func main() {
 	router.Use(static.Serve("/", static.EmbedFolder(frontend, "dist")))
 
 	router.GET("/api/ready", checkServer)
+
+	// Read index.html into memory
+	index, err := frontend.ReadFile("dist/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no route match is probably a vue route. So we return the index.html
+	// and the vue-router takes from here
+	router.NoRoute(func(c *gin.Context) {
+		if strings.HasPrefix(c.Request.URL.String(), "/api") {
+			c.JSON(http.StatusNotFound, "")
+			return
+		}
+		c.Data(http.StatusOK, "text/html", index)
+	})
 
 	router.Run(conf.Server.Bind)
 }
