8f2c19cbe870f628b32adcd7423533a86de9d61f
Fabio Murer <fabio.murer@studio.unibo.it>
2024-10-30 15:21:26 +0100

    test db

--- server/main.go
+++ server/main.go
@@ -2,14 +2,83 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+
+	// database
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
+
+func connect() (*sql.DB, error) {
+	bin, err := os.ReadFile("/run/secrets/db-password")
+	if err != nil {
+		return nil, err
+	}
+	return sql.Open("postgres", fmt.Sprintf("postgres://postgres:%s@db:5432/example?sslmode=disable", string(bin)))
+}
+
+func prepare() error {
+	db, err := connect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	for i := 0; i < 60; i++ {
+		if err := db.Ping(); err == nil {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+
+	if _, err := db.Exec("DROP TABLE IF EXISTS test"); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS test (id SERIAL, content VARCHAR)"); err != nil {
+		return err
+	}
+
+	for i := 0; i < 5; i++ {
+		if _, err := db.Exec("INSERT INTO test (content) VALUES ($1);", fmt.Sprintf("Blog post #%d", i)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getall(c *gin.Context) {
+	db, err := connect()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT content FROM test")
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	var contents []string
+	for rows.Next() {
+		var content string
+		_ = rows.Scan(&content)
+		contents = append(contents, content)
+	}
+
+	c.IndentedJSON(http.StatusOK, contents)
+}
 
 //go:embed dist
 var frontend embed.FS
@@ -33,6 +102,14 @@ func main() {
 	router.Use(static.Serve("/", static.EmbedFolder(frontend, "dist")))
 
 	router.GET("/api/ready", checkServer)
+
+	// test db
+	log.Print("Prepare db...")
+	if err := prepare(); err != nil {
+		log.Fatal(err)
+	}
+
+	router.GET("/api/db/", getall)
 
 	// Read index.html into memory
 	index, err := frontend.ReadFile("dist/index.html")
