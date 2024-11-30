5210682ab045764c8e133d2b1c909c68289f21fb
Lele <emanuele.2003@yahoo.it>
2024-11-30 08:00:13 +0100

    add: stats Item component

--- client/src/components/stats/StatItem.vue
+++ client/src/components/stats/StatItem.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="stat place-items-center">
-    <div class="stat-title">{{ title }}</div>
-    <div class="stat-value">{{ value }}</div>
+  <div :class="containerClass || 'stat'">
+    <div class="stat">
+      <div v-if="icon" class="stat-figure" :class="valueClass">
+        <i :class="[icon, 'text-2xl']"></i>
+      </div>
+      <div class="stat-title">{{ title }}</div>
+      <div class="stat-value" :class="valueClass">{{ value }}</div>
+      <div v-if="description" class="stat-desc">{{ description }}</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  title: string
-  value: string | number
-}>()
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: [String, Number],
+    required: true,
+  },
+  valueClass: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+  containerClass: {
+    type: String,
+    default: '',
+  },
+})
 </script>
