b17fd1a069795a3cdde9a54689925aa24bb798ab
Lele <emanuele.2003@yahoo.it>
2024-12-01 10:23:45 +0100

    add: rules modal

---
+++ client/src/components/RulesSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-primary">
+    <h3 class="mb-6 text-center text-3xl font-bold text-primary">
+      Backgammon Rules
+    </h3>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+</style>
