3382a5651463023fda4b3a0f2a09abb29215db87
Samuele Musiani <samu@teapot.ovh>
2024-12-12 13:13:07 +0100

    formatting

--- client/src/views/LoginView.spec.ts
+++ client/src/views/LoginView.spec.ts
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils';
-import { describe, it, expect } from 'vitest';
-import LoginView from './LoginView.vue';
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+import LoginView from './LoginView.vue'
 
 describe('LoginView', () => {
   it('should render', () => {
-    const wrapper = mount(LoginView);
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-});
+    const wrapper = mount(LoginView)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
