547663115296751d972f052d3e3cd7a2b10c48ff
Jabbar03 <ayache.omar@gmail.com>
2024-12-17 17:09:32 +0100

    tournament stats

--- server/handler/utils_test.go
+++ server/handler/utils_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestEloCalculation(t *testing.T) {
-	elo1, elo2 := calculateElo(1000, 1000, true)
+	elo1, elo2 := calculateElo(1000, 1000, true, 1)
 	assert.Equal(t, elo1, int64(1016))
 	assert.Equal(t, elo2, int64(984))
 }
